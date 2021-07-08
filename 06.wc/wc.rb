0f8709df676525f8a8916f7a277bca8e5ec40ee2
ShotaTsuru <syota.crane@gmail.com>
2021-07-08 15:07:48 +0900

    initializeメソッドにfile_satatusesを定義・実行処理の分岐を整理・totalの表示を追加

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -2,37 +2,8 @@
 # frozen_string_literal: true
 
 class Command
-  # オプションなしの出力
-  def wc_default(files)
-    file_extraction(files).each do |file|
-      puts "      #{file[:n_lines]}      #{file[:words]}     #{file[:byte_size]} #{file[:file_name]}"
-    end
-  end
-
-  def l_opt(files)
-    file_extraction(files).each do |file|
-      puts "      #{file[:n_lines]} #{file[:file_name]}"
-    end
-  end
-
-  def didnot_get_file(l: nil)
-    content = $stdin.read
-    file = {
-      n_lines: content.count("\n"), # 行数
-      words: content.split(' ').length, # 単語数
-      byte_size: content.bytesize # バイトサイズ
-    }
-    if l
-      puts "      #{file[:n_lines]}"
-    else
-      puts "       #{file[:n_lines]}      #{file[:words]}     #{file[:byte_size]}"
-    end
-  end
-
-  private
-
-  def file_extraction(files)
-    files.map.with_index do |file, i|
+  def initialize(files)
+    @file_statuses = files.map.with_index do |file, i|
       content = File.read(file)
       {
         n_lines: content.count("\n"), # 行数
@@ -40,6 +11,59 @@ class Command
         byte_size: content.bytesize, # バイトサイズ
         file_name: files[i]
       }
+    end
+  end
+
+  def exec_wc(option_l)
+    if @file_statuses.empty?
+      didnot_get_file(option_l)
+    else
+      option_l ? exec_default : exec_l_opt
+    end
+  end
+
+  private
+
+  # オプションなしの出力
+  def exec_default
+    @file_statuses.each do |file|
+      puts "      #{file[:n_lines]}      #{file[:words]}     #{file[:byte_size]} #{file[:file_name]}"
+    end
+    return unless @file_statuses.length > 1
+
+    total = { total_lines: 0, total_words: 0, total_byte_size: 0 }
+    @file_statuses.each do |file|
+      total[:total_lines] += file[:n_lines]
+      total[:total_words] += file[:words]
+      total[:total_byte_size] += file[:byte_size]
+    end
+    puts "      #{total[:total_lines]}      #{total[:total_words]}     #{total[:total_byte_size]} total"
+  end
+
+  def exec_l_opt
+    @file_statuses.each do |file|
+      puts "      #{file[:n_lines]} #{file[:file_name]}"
+    end
+    return unless @file_statuses.length > 1
+
+    total = { total_lines: 0, total_words: 0, total_byte_size: 0 }
+    @file_statuses.each do |file|
+      total[:total_lines] += file[:n_lines]
+    end
+    puts "      #{total[:total_lines]} total"
+  end
+
+  def didnot_get_file(option_l)
+    content = $stdin.read
+    file = {
+      n_lines: content.count("\n"), # 行数
+      words: content.split(' ').length, # 単語数
+      byte_size: content.bytesize # バイトサイズ
+    }
+    if option_l
+      puts "      #{file[:n_lines]}"
+    else
+      puts "       #{file[:n_lines]}      #{file[:words]}     #{file[:byte_size]}"
     end
   end
 end
@@ -55,12 +79,6 @@ if $PROGRAM_NAME == __FILE__
   opt.parse!(ARGV)
   files = ARGV
 
-  command_receiver = Command.new
-  if params[:l] && !ARGV.empty?
-    command_receiver.l_opt(files)
-  elsif ARGV.empty?
-    command_receiver.didnot_get_file(**params)
-  else
-    command_receiver.wc_default(files)
-  end
+  command_receiver = Command.new(files)
+  command_receiver.exec_wc(params[:l])
 end
