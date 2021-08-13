c01d3c41248da75583a6043ca388b60162c3d215
ShotaTsuru <syota.crane@gmail.com>
2021-08-13 17:46:48 +0900

    fileクラス作成

--- 08.ls_object/file.rb
+++ 08.ls_object/file.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'etc'
+
 class File
+  attr_reader :permission, :nlink, :user, :group, :size, :time, :file_name
+  def initialize
+  end
 
 end
+
+file = File.new
